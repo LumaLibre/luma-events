fbd3d5f5553f4b431db0e3955fc21ac5dce39f03
Jsinco <jonah@jsinco.dev>
2025-04-30 10:30:57 -0400

    fix quiz reward

--- src/main/java/dev/jsinco/luma/lumaevents/npc/constants/TutorialSection.java
+++ src/main/java/dev/jsinco/luma/lumaevents/npc/constants/TutorialSection.java
@@ -51,6 +51,12 @@ public enum TutorialSection {
             "I was just about to go on a little adventure myself...",
             "Then I got lost down this rabbit hole and now I'm stuck.",
             "Here, take this and go find some help..."
+    ),
+    QUIZ_REWARD_FIXED(
+            "Oh? How did you find me all the way down here?",
+            "I was just about to go on a little adventure myself...",
+            "Then I got lost down this rabbit hole and now I'm stuck.",
+            "Here, take this and go find some help..."
     );
 
     private final String[] lines;
