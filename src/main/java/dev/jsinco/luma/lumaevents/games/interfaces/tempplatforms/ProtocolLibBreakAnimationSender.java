197e1d69dd1b20db8edd2e9640b5f0571e8a3532
Mitality <96748385+Mitality@users.noreply.github.com>
2026-01-20 23:11:42 +0100

    Use noop platform breaking animation if ProtocolLib isn't enabled

--- src/main/java/dev/jsinco/luma/lumaevents/games/interfaces/tempplatforms/ProtocolLibBreakAnimationSender.java
+++ src/main/java/dev/jsinco/luma/lumaevents/games/interfaces/tempplatforms/ProtocolLibBreakAnimationSender.java
@@ -5,11 +5,12 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.BlockPosition;
 import org.bukkit.entity.Player;
+import org.checkerframework.common.value.qual.IntRange;
 
 public final class ProtocolLibBreakAnimationSender implements BreakAnimationSender {
 
     @Override
-    public void sendBreakAnimation(Player viewer, int breakerId, int x, int y, int z, int stage) {
+    public void sendBreakAnimation(Player viewer, int breakerId, int x, int y, int z, @IntRange(from = -1, to = 9) int stage) {
         PacketContainer packet = ProtocolLibrary.getProtocolManager()
                 .createPacket(PacketType.Play.Server.BLOCK_BREAK_ANIMATION);
         packet.getIntegers().write(0, breakerId);
